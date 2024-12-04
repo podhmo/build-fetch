4b63bebf0ef8dcef29c4b5e053a3694e2c262388
podhmo <ababjam61+github@gmail.com>
2024-12-04 11:24:30 +0900

    chore: add exapmles

---
+++ examples/specialized.ts
@@ -0,0 +1,16 @@
+import { SpecializedFetch } from "../mod.ts";
+
+const baseUrl = "https://httpbin.org";
+const endpoints = [
+  `${baseUrl}/status/200`,
+  `${baseUrl}/status/404`,
+  `${baseUrl}/status/500`,
+] as const;
+
+const fetch: SpecializedFetch<typeof endpoints[number]> = globalThis.fetch;
+
+// [deno-ts] Argument of type '"https://example.net"' is not assignable to parameter of type '"https://httpbin.org/status/200" | "https://httpbin.org/status/404" | "https://httpbin.org/status/500"'.
+// fetch("https://example.net");
+
+const response = await fetch("https://httpbin.org/status/200");
+console.log(response.status);
